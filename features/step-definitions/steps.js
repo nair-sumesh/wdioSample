ed33ec68562d2246bfa84c2fdfc8453178b84901
I060573 <sumesh.nair@sap.com>
2024-02-29 12:30:17 -0500

    first draft

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,27 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { expect, $ } = require('@wdio/globals')
+
+const LoginPage = require('../../login.page');
+const SecurePage = require('../../secure.page');
+
+const pages = {
+    login: LoginPage
+}
+
+Given(/^I am on the (\w+) page$/, async (page) => {
+    await pages[page].open()
+});
+
+When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+    
+    await browser.setPermissions({ name: 'clipboard-read' }, 'granted');
+    await browser.setPermissions({ name: 'clipboard-write' }, 'granted');
+    
+    await LoginPage.login(username, password)
+});
+
+Then(/^I should see a flash message saying (.*)$/, async (message) => {
+    await expect(SecurePage.flashAlert).toBeExisting();
+    await expect(SecurePage.flashAlert).toHaveTextContaining(message);
+});
+
